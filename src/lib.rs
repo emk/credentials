8bd8da65c8f38db1150be1840d74bf5463770f64
Eric Kidd <git@randomhacks.net>
2016-03-19 08:57:23 -0400

    Provide a basic wrapper around std::env
    This is just a starting point, but it should already work fine for
    12factor applications that use environment variables to pass secrets.
    
    http://12factor.net/

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,23 @@
-#[cfg(test)]
-mod test {
-    #[test]
-    fn it_works() {
-    }
+//! Access secure credentials at runtime with multiple backends.
+//!
+//! ```
+//! use credentials;
+//! use std::env;
+//!
+//! env::set_var("PASSWORD", "secret");
+//! assert_eq!("secret", credentials::get("PASSWORD").unwrap());
+//! ```
+
+use std::convert::AsRef;
+use std::env;
+
+/// An error occurred accessing credentials.
+#[derive(Clone, Copy, Debug)]
+pub struct CredentialError;
+
+/// Fetch the value of a credential.
+pub fn get<K: AsRef<str>>(key: K) -> Result<String, CredentialError> {
+    env::var(key.as_ref()).map_err(|_| {
+        CredentialError
+    })
 }
